bccf8e275e26f6abc969ba889996937f07077086
Emad Shaaban <emad.shaaban92@gmail.com>
2017-06-17 16:15:05 +0200

    auth async await

--- actions/auth.js
+++ actions/auth.js
@@ -1,77 +1,94 @@
 import RNKunafa from '../RNKunafa';
 
-import {reLoadDoc} from './documents';
+import {
+  reLoadDoc
+} from './documents';
 
 export const skipLogin = () => {
-	return {type: 'SKIP_LOGIN'}
+  return {
+    type: 'SKIP_LOGIN'
+  }
 }
 
-function login(hostUrl, user) {
-	//console.log(user);
-	return fetch(hostUrl + "/_session", {
-		method: 'POST',
-		body: JSON.stringify(user),
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		}
-	}).then(function(response) {
-		//console.log(response);
-		if (response.status == 200) {
-			return response.json();
-		} else {
-			console.log(response);
-			throw new Error("Login Error");
-		}
-	})
+const login = async(hostUrl, user) => {
+  //console.log(user);
+  const response = await fetch(hostUrl + "/_session", {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    console.log(response);
+    throw new Error("Login Error");
+  }
 }
 
-function fetchUser(hostUrl, user_id) {
-	const url = hostUrl + "/_users/" + user_id;
-	return fetch(url, {
-		method: 'GET',
-		headers: {
-			'Accept': 'application/json'
-		}
-	}).then(function(response) {
-		if (response.status == 200) {
-			return response.json();
-		} else {
-			throw new Error("Request Error");
-		}
-	})
+const fetchUser = async(hostUrl, user_id) => {
+  const url = hostUrl + "/_users/" + user_id;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new Error("Request Error");
+  }
+}
+
+const auth = async(name, password) => {
+  const session = await login(`http://${RNKunafa.host}`, {
+    name,
+    password
+  });
+  const userId = 'org.couchdb.user:' + session.name;
+  return await fetchUser(`http://${RNKunafa.host}`, userId);
 }
 
 export const startLoading = () => {
-	return {type: 'START_LOADING'}
+  return {
+    type: 'START_LOADING'
+  }
 }
 
 export const userLogin = (name, password) => {
-	return dispatch => {
-		dispatch({type: 'START_LOADING'})
-		login(`http://${RNKunafa.host}`, {name, password}).then((user) => {
-			//console.log(user)
-			const userId = 'org.couchdb.user:' + user.name;
-			return fetchUser(`http://${RNKunafa.host}`, userId);
-		}).then(user => {
-			RNKunafa.login(name, password, user.profileId);
-			return user;
-		}).then(user => {
-			dispatch(reLoadDoc({_id: user.profileId}));
-			RNKunafa.onLogin(() => {
-				dispatch({type: 'LOGIN', profileId: user.profileId})
-			})
-		})
-	}
+  return dispatch => {
+    dispatch({
+      type: 'START_LOADING'
+    })
+    auth(name, password).then(user => {
+      RNKunafa.login(name, password, user.profileId);
+      dispatch(reLoadDoc({
+        _id: user.profileId
+      }));
+      RNKunafa.onLogin(() => {
+        dispatch({
+          type: 'LOGIN',
+          profileId: user.profileId
+        })
+      })
+    }).catch(e => {
+      console.log(e);
+    });
+  }
 }
 
 export const userLogout = () => {
-	return dispatch => {
-		dispatch({type: 'START_LOADING'});
-		RNKunafa.logout();
-		RNKunafa.onLogout(() => {
-			RNRestart.Restart();
-		})
-	}
+  return dispatch => {
+    dispatch({
+      type: 'START_LOADING'
+    });
+    RNKunafa.logout();
+    RNKunafa.onLogout(() => {
+      RNRestart.Restart();
+    })
+  }
 
 }
--- actions/documents.js
+++ actions/documents.js
@@ -1,40 +1,40 @@
 import RNKunafa from '../RNKunafa';
 
 export const loadDocs = (query, overrideAction = {}) => dispatch => {
-	return RNKunafa.publicDb.find(query).then(result => {
-		return result.docs.map(doc => {
-			return {
-				...doc,
-				fetchedAt: Date.now()
-			}
-		})
-	}).then(docs => {
-		if (docs && docs.length > 0) {
-			dispatch({
-				type: 'LOAD_DOCS',
-				docs,
-				...overrideAction
-			});
-		}
-	}).catch(e => {
-		console.log(e);
-	});
+  return RNKunafa.publicDb.find(query).then(result => {
+    return result.docs.map(doc => {
+      return {
+        ...doc,
+        fetchedAt: Date.now()
+      }
+    })
+  }).then(docs => {
+    if (docs && docs.length > 0) {
+      dispatch({
+        type: 'LOAD_DOCS',
+        docs,
+        ...overrideAction
+      });
+    }
+  }).catch(e => {
+    console.log(e);
+  });
 }
 
 const TTL = 5 * 60 * 1000; //5 minuts
 //const TTL = 0; // Live Update
 
 export const reLoadDoc = (doc, overrideAction = {}) => dispatch => {
-	if (!doc || !doc._id) {
-		return Promise.resolve();
-	}
-	if (!doc.invalidCache && doc.fetchedAt && (Date.now() - doc.fetchedAt < TTL)) {
-		//console.log("loaded from cache");
-		return Promise.resolve();
-	}
-	dispatch(loadDocs({
-		selector: {
-			_id: doc._id
-		}
-	}, overrideAction));
+  if (!doc || !doc._id) {
+    return Promise.resolve();
+  }
+  if (!doc.invalidCache && doc.fetchedAt && (Date.now() - doc.fetchedAt < TTL)) {
+    //console.log("loaded from cache");
+    return Promise.resolve();
+  }
+  dispatch(loadDocs({
+    selector: {
+      _id: doc._id
+    }
+  }, overrideAction));
 }
