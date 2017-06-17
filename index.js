77c3b3cb77c1fbbd960d139328c383c32ec8a7a3
Emad Shaaban <emad.shaaban92@gmail.com>
2017-06-17 04:39:22 +0200

    process.nextTick

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+require('blob-polyfill');
+window.atob = require('base-64').decode;
+window.btoa = require('base-64').encode;
+process.nextTick = setImmediate;
+
 import RNKunafa from './RNKunafa';
 import createApp from './createApp';
 import createStore from './createStore';
