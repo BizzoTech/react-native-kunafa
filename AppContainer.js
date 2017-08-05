6a0eb84b2b1584815a1632d6d7cb422c553cd331
Emad Shaaban <emad.shaaban92@gmail.com>
2017-08-05 03:34:21 +0200

    refactor AppContainer to class componenet

--- AppContainer.js
+++ AppContainer.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    StatusBar,
-		Linking,
-    Dimensions
+ StyleSheet,
+ Text,
+ View,
+ StatusBar,
+ Linking,
+ Dimensions
 } from 'react-native';
 import R from 'ramda';
-import PopupDialog, { DialogTitle, SlideAnimation } from 'react-native-popup-dialog';
+import PopupDialog, {DialogTitle, SlideAnimation} from 'react-native-popup-dialog';
 
 import * as Progress from 'react-native-progress';
 
@@ -18,52 +18,50 @@ import connect from './connect';
 
 import RNKunafa from './RNKunafa';
 
-const AppContainer = React.createClass({
-		renderActivityIndicator(){
-			const {processingLocal} = this.props;
-			const color = RNKunafa.appConfig.progressBarColor(RNKunafa.AppStore) //'#E91E62' // theme.color1;
-			if(processingLocal.isProcessing){
-				return <Progress.Bar color={color} progress={processingLocal.progress} indeterminate={!processingLocal.progress} width={Dimensions.get('window').width} />;
-			}
-		},
-    renderDialog(){
-      const {dialog, closeDialog} = this.props;
-      if(dialog.currentDialog){
-        return (
-          <PopupDialog height={dialog.height || height} onDismissed={closeDialog} show={true}
-             ref={popup => {this.popup = popup}}
-             dialogTitle={dialog.title ? <DialogTitle title={dialog.title} /> : undefined}
-             dialogAnimation={dialog.animated ? new SlideAnimation({slideFrom: 'bottom', animationDuration:300}) : undefined}>
-  					{this.renderDialogContent()}
-  				</PopupDialog>
-        )
-      }
-    },
-    renderDialogContent(){
-      const {dialog, closeDialog} = this.props;
-      return RNKunafa.appConfig.renderDialogContent(dialog, closeDialog);
-    },
-    render(){
-      const {Main, route} = this.props;
-			const color = RNKunafa.appConfig.statusBarColor(RNKunafa.AppStore)// '#E2004C'; // theme.color17;
-      return (
-        <View style={{flex: 1}}>
-         <StatusBar
-           backgroundColor={color} animated={true}
-         />
-         <Main route={route} />
-				 {this.renderActivityIndicator()}
-
-				 {this.renderDialog()}
-       </View>
-      )
-    }
-});
+class AppContainer extends Component {
+ renderActivityIndicator = () => {
+  const {processingLocal} = this.props;
+  const color = RNKunafa.appConfig.progressBarColor(RNKunafa.AppStore);
+  if (processingLocal.isProcessing) {
+   return <Progress.Bar color={color} progress={processingLocal.progress} indeterminate={!processingLocal.progress} width={Dimensions.get('window').width}/>;
+  }
+ }
+ renderDialog = () => {
+  const {dialog, closeDialog} = this.props;
+  if (dialog.currentDialog) {
+   return (
+    <PopupDialog height={dialog.height || height} onDismissed={closeDialog} show={true} ref={popup => {
+     this.popup = popup
+    }} dialogTitle={dialog.title
+     ? <DialogTitle title={dialog.title}/>
+     : undefined} dialogAnimation={dialog.animated
+     ? new SlideAnimation({slideFrom: 'bottom', animationDuration: 300})
+     : undefined}>
+     {this.renderDialogContent()}
+    </PopupDialog>
+   )
+  }
+ }
+ renderDialogContent = () => {
+  const {dialog, closeDialog} = this.props;
+  return RNKunafa.appConfig.renderDialogContent(dialog, closeDialog);
+ }
+ render = () => {
+  const {Main, route} = this.props;
+  const color = RNKunafa.appConfig.statusBarColor(RNKunafa.AppStore)
+  return (
+   <View style={{
+    flex: 1
+   }}>
+    <StatusBar backgroundColor={color} animated={true}/>
+    <Main route={route}/>
+    {this.renderActivityIndicator()}
+    {this.renderDialog()}
+   </View>
+  )
+ }
+}
 
 export default connect(state => {
-  	return {
-      processingLocal: state.processing_local,
-      route: state.history[0],
-			dialog: state.dialog
-    }
+ return {processingLocal: state.processing_local, route: state.history[0], dialog: state.dialog}
 })(AppContainer);
