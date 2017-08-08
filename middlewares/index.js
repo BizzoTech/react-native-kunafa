c3b8c5dc96516722f055833ce7236b62c592eaee
Emad Shaaban <emad.shaaban92@gmail.com>
2017-08-08 16:17:36 +0200

    upgrade kunafa client

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,6 +3,4 @@ import eventSourcingMiddleware from './event_sourcing_middleware';
 import localCacheMiddleware from './local_cache_middleware';
 import processLocalEventsMiddleware from './process_local_events_middleware';
 
-import eventChangeHandlerMiddleware from './event_change_handler_middleware';
-
-export default [processLocalEventsMiddleware, localCacheMiddleware, eventSourcingMiddleware, syncMiddleware, eventChangeHandlerMiddleware];
+export default [processLocalEventsMiddleware, localCacheMiddleware, eventSourcingMiddleware, syncMiddleware];
